eb888d9a7c01c6e59551bbc32113fabfb0016a80
krrishnaaaa <krishna@pcsalt.com>
2017-10-13 07:22:28 +0530

    display rate dialog

--- app/src/main/java/com/pcsalt/example/alertdialogdemo/MainActivity.java
+++ app/src/main/java/com/pcsalt/example/alertdialogdemo/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btn_show_dialog).setOnClickListener(view -> showDialog());
+        findViewById(R.id.btn_show_dialog).setOnClickListener(view -> rateDialog());
     }
 
     /*
@@ -23,5 +23,32 @@ public class MainActivity extends AppCompatActivity {
         dialogBuilder.setMessage("Some informative message for the user.");
         dialogBuilder.setPositiveButton("Done", (dialog, which) -> dialog.dismiss());
         dialogBuilder.create().show();
+    }
+
+    /*
+    display rate dialog
+     */
+    private void rateDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("Rate Us");
+        dialogBuilder.setMessage("If you liked it, please rate it." +
+                " If you do not like it rate it." +
+                " It will help us grow.");
+        dialogBuilder.setPositiveButton("Rate", (dialog, which) -> {
+            // take user to the page to rate
+            // and dismiss the dialog
+            dialog.dismiss();
+        });
+
+        dialogBuilder.setNegativeButton("Leave it", (dialog, which) -> {
+            // set a flag in system, that user do not want to rate
+            // and dismiss the dialog
+            dialog.dismiss();
+        });
+
+        dialogBuilder.setNeutralButton("May be, later", (dialog, which) -> dialog.dismiss());
+
+        dialogBuilder.create().show();
+
     }
 }
