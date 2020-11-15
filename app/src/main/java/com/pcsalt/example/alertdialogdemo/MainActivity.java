0f831328207984ea537c38aedb1221982de2718c
krrishnaaaa <krishna@pcsalt.com>
2020-11-16 00:32:34 +0530

    updated dependencies

--- app/src/main/java/com/pcsalt/example/alertdialogdemo/MainActivity.java
+++ app/src/main/java/com/pcsalt/example/alertdialogdemo/MainActivity.java
@@ -1,9 +1,10 @@
 package com.pcsalt.example.alertdialogdemo;
 
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
